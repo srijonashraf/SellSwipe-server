2d589a6133ab0be23476512d32437c6fcf34c92e
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-04-28 11:40:41 +0600

    api updated

--- src/routes/user.api.js
+++ src/routes/user.api.js
@@ -4,16 +4,18 @@ import * as UserController from "../controllers/UserController.js";
 import { upload } from "../middlewares/MulterMiddleware.js";
 import AuthVerifyMiddlware from "../middlewares/AuthVerifyMiddleware.js";
 import { checkLoginAttempts } from './../middlewares/LoginAttemptWatcherMiddleware.js';
+import { checkEmailVerification } from "../middlewares/EmailVerficationCheckMiddleware.js";
+import { checkNidVerification } from './../middlewares/NidVerificationCheckMiddleware.js';
 const userRouter = express.Router();
 
 userRouter.post("/registration", UserController.userRegistration);
-userRouter.post("/login", checkLoginAttempts, UserController.userLogin);
+userRouter.post("/login", checkLoginAttempts, checkEmailVerification, UserController.userLogin);
 userRouter.get("/verifyEmail", UserController.userEmailVerify);
 userRouter.get("/verifyOTP", UserController.OTPVerify);
 userRouter.post("/resetPassword", UserController.recoverResetPassword);
 userRouter.post("/updateProfilePhoto", AuthVerifyMiddlware, upload.any(), UserController.userProfilePhotoUpdate);
 userRouter.post("/updateProfile", AuthVerifyMiddlware, UserController.userProfileUpdate);
 userRouter.get("/profileDetails", AuthVerifyMiddlware, UserController.userProfileDetails);
-userRouter.post("/createPost", AuthVerifyMiddlware, PostController.CreatePost);
+userRouter.post("/createPost", AuthVerifyMiddlware, checkNidVerification, PostController.CreatePost);
 
 export default userRouter;

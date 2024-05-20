a8148bf71fad112808796db2a9e4a5b692c438e5
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-05-20 10:26:50 +0600

    updated api

--- src/routes/user.api.js
+++ src/routes/user.api.js
@@ -6,6 +6,7 @@ import AuthVerifyMiddlware from "../middlewares/AuthVerifyMiddleware.js";
 import { checkLoginAttempts } from './../middlewares/LoginAttemptWatcherMiddleware.js';
 import { checkEmailVerification } from "../middlewares/EmailVerficationCheckMiddleware.js";
 import { checkNidVerification } from './../middlewares/NidVerificationCheckMiddleware.js';
+import { checkAccountStatus } from "../middlewares/AccountStatusCheckMiddleware.js";
 const userRouter = express.Router();
 
 userRouter.post("/registration", UserController.userRegistration);
@@ -15,13 +16,13 @@ userRouter.get("/verifyOTP", UserController.OTPVerify);
 userRouter.post("/resetPassword", UserController.recoverResetPassword);
 userRouter.post("/updateAvatar", AuthVerifyMiddlware, upload.single("avatar"), UserController.userAvatarUpdate);
 userRouter.post("/updateProfile", AuthVerifyMiddlware, UserController.userProfileUpdate);
-userRouter.post("/updateNidRequest", AuthVerifyMiddlware,upload.fields([{ name: 'nidFront', maxCount: 1 }, { name: 'nidBack', maxCount: 8 }]),UserController.userNidUpdateRequest);
-userRouter.get("/profileDetails", AuthVerifyMiddlware, UserController.userProfileDetails);
+userRouter.post("/updateNidRequest", AuthVerifyMiddlware,upload.fields([{ name: 'nidFront', maxCount: 1 }, { name: 'nidBack', maxCount: 1 }]),UserController.userNidUpdateRequest);
+userRouter.get("/profileDetails", AuthVerifyMiddlware, checkAccountStatus, UserController.userProfileDetails);
 userRouter.get("/allSession", AuthVerifyMiddlware, UserController.userAllSession);
 userRouter.get("/logoutFromSession", AuthVerifyMiddlware, UserController.userLogoutFromSession);
 
 //Temporariry giving access without checkNidVerficaion middleware for test
 //Could not sent both img file and json body to api request from Postman for this using a index.html to upload both of them
-userRouter.post("/createPost", AuthVerifyMiddlware, upload.array('images', 5), PostController.CreatePost);
+userRouter.post("/createPost", AuthVerifyMiddlware, checkAccountStatus, upload.array('images', 5), PostController.CreatePost);
 
 export default userRouter;

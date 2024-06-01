d4cb2ef6e65d8a7f7443e7216c6009b0bedf367a
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-06-01 23:30:41 +0600

    update post added

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -3,9 +3,15 @@ import {
   deletePostDetailsImage,
   deletePostService,
   postListService,
+  updatePostService,
 } from "./../services/PostServices.js";
 export const CreatePost = async (req, res) => {
   let result = await createPostService(req);
+  return res.status(200).json(result);
+};
+
+export const UpdatePost = async (req, res) => {
+  let result = await updatePostService(req);
   return res.status(200).json(result);
 };
 
@@ -18,7 +24,6 @@ export const DeletePostImage = async (req, res) => {
   let result = await deletePostDetailsImage(req);
   return res.status(200).json(result);
 };
-
 
 export const PostList = async (req, res) => {
   let result = await postListService(req);

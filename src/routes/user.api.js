6b72a8a8b494c527b1b4f36b8293675612fee753
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-06-01 23:32:04 +0600

    validation added

--- src/routes/user.api.js
+++ src/routes/user.api.js
@@ -3,29 +3,103 @@ import * as PostController from "../controllers/PostController.js";
 import * as UserController from "../controllers/UserController.js";
 import { upload } from "../middlewares/MulterMiddleware.js";
 import AuthVerifyMiddlware from "../middlewares/AuthVerifyMiddleware.js";
-import { checkLoginAttempts } from './../middlewares/LoginAttemptWatcherMiddleware.js';
+import { checkLoginAttempts } from "./../middlewares/LoginAttemptWatcherMiddleware.js";
 import { checkEmailVerification } from "../middlewares/EmailVerficationCheckMiddleware.js";
-import { checkNidVerification } from './../middlewares/NidVerificationCheckMiddleware.js';
+import { checkNidVerification } from "./../middlewares/NidVerificationCheckMiddleware.js";
 import { checkAccountStatus } from "../middlewares/AccountStatusCheckMiddleware.js";
+import { validateRequest } from "../middlewares/RequestValidateMiddleware.js";
+import { postSchemaCreate } from "./../request/post.schema.js";
+import { userSchemaCreate } from "../request/user.schema.js";
+
 const userRouter = express.Router();
 
 userRouter.post("/registration", UserController.userRegistration);
-userRouter.post("/login", checkLoginAttempts, checkEmailVerification, UserController.userLogin);
+
+userRouter.post(
+  "/login",
+  checkLoginAttempts,
+  checkEmailVerification,
+  validateRequest({ schema: userSchemaCreate, isParam: false, isQuery: false }),
+  UserController.userLogin
+);
+
 userRouter.get("/verifyEmail", UserController.userEmailVerify);
+
 userRouter.get("/verifyOTP", UserController.OTPVerify);
+
 userRouter.post("/resetPassword", UserController.recoverResetPassword);
-userRouter.post("/updateAvatar", AuthVerifyMiddlware, upload.single("avatar"), UserController.userAvatarUpdate);
-userRouter.post("/updateProfile", AuthVerifyMiddlware, UserController.userProfileUpdate);
-userRouter.post("/updateNidRequest", AuthVerifyMiddlware,upload.fields([{ name: 'nidFront', maxCount: 1 }, { name: 'nidBack', maxCount: 1 }]),UserController.userNidUpdateRequest);
-userRouter.get("/profileDetails", AuthVerifyMiddlware, checkAccountStatus, UserController.userProfileDetails);
-userRouter.get("/allSession", AuthVerifyMiddlware, UserController.userAllSession);
-userRouter.get("/logoutFromSession", AuthVerifyMiddlware, UserController.userLogoutFromSession);
+
+userRouter.post(
+  "/updateAvatar",
+  AuthVerifyMiddlware,
+  upload.single("avatar"),
+  UserController.userAvatarUpdate
+);
+
+userRouter.post(
+  "/updateProfile",
+  AuthVerifyMiddlware,
+  UserController.userProfileUpdate
+);
+
+userRouter.post(
+  "/updateNidRequest",
+  AuthVerifyMiddlware,
+  upload.fields([
+    { name: "nidFront", maxCount: 1 },
+    { name: "nidBack", maxCount: 1 },
+  ]),
+  UserController.userNidUpdateRequest
+);
+
+userRouter.get(
+  "/profileDetails",
+  AuthVerifyMiddlware,
+  checkAccountStatus,
+  UserController.userProfileDetails
+);
+
+userRouter.get(
+  "/allSession",
+  AuthVerifyMiddlware,
+  UserController.userAllSession
+);
+
+userRouter.get(
+  "/logoutFromSession",
+  AuthVerifyMiddlware,
+  UserController.userLogoutFromSession
+);
 
 //Temporariry giving access without checkNidVerficaion middleware for test
-//Could not sent both img file and json body to api request from Postman for this using a index.html to upload both of them
-userRouter.post("/createPost", AuthVerifyMiddlware, checkAccountStatus, upload.array('images', 5), PostController.CreatePost);
-userRouter.post("/updatePost", AuthVerifyMiddlware, checkAccountStatus,upload.array('images', 5), PostController.CreatePost);
+userRouter.post(
+  "/createPost",
+  AuthVerifyMiddlware,
+  checkAccountStatus,
+  upload.array("images", 5),
+  validateRequest({ schema: postSchemaCreate, isQuery: false, isParam: false }),
+  PostController.CreatePost
+);
+
+userRouter.post(
+  "/updatePost",
+  AuthVerifyMiddlware,
+  validateRequest({ isQuery: false }),
+  upload.array("images", 5),
+  PostController.UpdatePost
+);
+
 userRouter.get("/deletePost", AuthVerifyMiddlware, PostController.DeletePost);
-userRouter.get("/deletePostImage", AuthVerifyMiddlware, PostController.DeletePostImage);
+
+userRouter.get(
+  "/deletePostImage",
+  AuthVerifyMiddlware,
+  PostController.DeletePostImage
+);
+
+userRouter.get(
+  "/testValidator",
+  validateRequest({ schema: postSchemaCreate, isQuery: false, isParam: false })
+);
 
 export default userRouter;

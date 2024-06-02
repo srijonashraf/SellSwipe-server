116e5bf206e852c3580365fbcef626cdc1f6459a
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-06-02 23:16:19 +0600

    deleted

--- src/middlewares/AccountStatusCheckMiddleware.js
+++
@@ -1,8 +0,0 @@
-//This middleware is used to check if the accountStatus is Validate then user will be able to post.
-
-export const checkAccountStatus = async (req, res, next) => {
-  const userID = req.headers.id || req.cookies.id;
-//   console.log(userID);
-
-  next();
-};
